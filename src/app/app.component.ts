44f4410f0acc53ca5c326e40ebf6eb09dddfba1b
Alcel Marie Obaña <obanaalcel@gmail.com>
2024-05-14 22:43:20 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Login', url: '/login', icon: 'mail' },
+    { title: 'Register', url: '/register', icon: 'paper-plane' },
+  ];
+  constructor() {}
+}
